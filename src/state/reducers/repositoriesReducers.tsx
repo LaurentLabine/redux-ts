079a1c29f9552f2af5e91637b96192c831c50344
Laurent Labine <laurentlabine@gmail.com>
2021-06-02 16:22:54 +0200

    Implemented reducer
    Mostly plain javascript for now.

---
+++ src/state/reducers/repositoriesReducers.tsx
@@ -0,0 +1,21 @@
+interface RepositariesState {
+  loading: boolean
+  error: string | null
+  data: string[]
+}
+
+const reducer = (state: RepositariesState, action: any) => {
+  switch(action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: []}
+    case 'search_repositories_success':
+      return {loading: false, error: null, data: action.payload}
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] }
+    default:
+      return state
+  }
+
+}
+
+export default reducer
