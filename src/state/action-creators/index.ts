cae10a3955f9d6c6ebadfa417e5920c477197349
Laurent Labine <laurentlabine@gmail.com>
2021-06-02 22:36:13 +0200

    Implemented Action Creator
    lesson no.33

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,13 +2,29 @@ import axios from 'axios'
 import { ActionType } from '../action-types'
 import { Action } from '../actions'
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: any) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     })
 
     try {
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', 
+        {
+          params: {
+          text: term
+        }
+      })
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      })
+
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names
+      })
       
     } catch (err) {
       dispatch({
