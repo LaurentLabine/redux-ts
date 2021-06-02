93505e8742bddf53252c4a723397d40934f6d039
Laurent Labine <laurentlabine@gmail.com>
2021-06-02 16:54:23 +0200

    Defined different interfaces for different action types and enum to enumerate actions

--- src/state/reducers/repositoriesReducers.tsx
+++ src/state/reducers/repositoriesReducers.tsx
@@ -1,16 +1,50 @@
+// ref to Course section 4
 interface RepositariesState {
   loading: boolean
   error: string | null
   data: string[]
 }
 
-const reducer = (state: RepositariesState, action: any): RepositariesState => {
-  switch(action.type) {
-    case 'search_repositories':
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+  payload: string[]
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR
+  payload: string
+}
+
+type Action = 
+  | SearchRepositoriesAction 
+  | SearchRepositoriesSuccessAction 
+  | SearchRepositoriesErrorAction
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+const reducer = (
+  state: RepositariesState, 
+  action: Action
+
+  ): RepositariesState => {
+
+
+  switch(action.type) { //Using Typeguard we can determine accordingly to the case what the type of action.payload is.
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: []}
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      //According to interface definition, payload here is an array of strings
       return {loading: false, error: null, data: action.payload}
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      //Here payload holds a string.
       return { loading: false, error: action.payload, data: [] }
     default:
       return state
