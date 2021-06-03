e451f5dc7f74b99eecea085a564e19a3bd691c01
Laurent Labine <laurentlabine@gmail.com>
2021-06-03 10:43:53 +0200

    Added comments and RootState type anotation export

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -2,7 +2,10 @@ import { combineReducers } from 'redux'
 import repositoriesReducer from './repositoriesReducer'
 
 const reducers = combineReducers({
-  repositories: repositoriesReducer
+  repositories: repositoriesReducer //repositories key : repositories reducer
 })
 
 export default reducers
+
+//We exporting a type called RootState to specify that the RootState type is set to whatever the reducers function returns
+export type RootState = ReturnType<typeof reducers>
