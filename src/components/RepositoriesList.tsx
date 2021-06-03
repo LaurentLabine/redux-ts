337da393e689b685dc21222b11a5238269ffd2bc
Laurent Labine <laurentlabine@gmail.com>
2021-06-03 10:44:23 +0200

    Implemented logic to display on my component the returned values

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 import { useActions } from '../hooks/useActions'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions()
+  const { data, error, loading} = useTypedSelector(
+    (state) => state.repositories
+    )
 
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -18,6 +22,10 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={e => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+      data.map((name)=> <div key={name}>{name}</div>)}
     </div>
   )
 }
